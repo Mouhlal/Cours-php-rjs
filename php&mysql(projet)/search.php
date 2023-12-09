93003c8e55edc372e00324fe31e44bd1e515e367
Mouhlal Akram <akramemouhlal@gmail.com>
2023-12-09 01:49:49 +0100

    modifier page name

--- php&mysql(projet)/search.php
+++ php&mysql(projet)/search.php
@@ -7,7 +7,7 @@ require "con.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Search</title>
+    <title>Search Player</title>
 </head>
 <style>
     body{
